1e8f1013e8b0ec7fd29a1b3c0dcdc59d9a625b8a
Owen K Davey <9116900+noflashbang@users.noreply.github.com>
2025-02-18 11:30:13 -0800

    removed broken heap and added default

---
+++ src/RogueSyntaxCompiler/src/example/default.mk
@@ -0,0 +1,19 @@
+//set up some variables
+let five = 5;
+let ten = 10;
+let strHello = "hello";
+let strWorld = "world";
+
+//function
+let add = fn(x, y) { x + y; };
+
+//call function
+let result = add(five, ten);
+
+//external call
+printLine(result);
+
+//use add function on strings
+let strResult = add(strHello, strWorld);
+printLine(strResult); 
+
